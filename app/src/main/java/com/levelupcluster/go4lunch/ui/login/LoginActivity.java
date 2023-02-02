d686a9d61dd3585dcad7201c77228c69e2e42fc3
pgaillot <p.gaillot@essteam.fr>
2023-02-02 13:59:50 +0100

    connexion update

---
+++ app/src/main/java/com/levelupcluster/go4lunch/ui/login/LoginActivity.java
@@ -0,0 +1,100 @@
+package com.levelupcluster.go4lunch.ui.login;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.levelupcluster.go4lunch.MainActivity;
+import com.levelupcluster.go4lunch.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        Button connexion_btn = findViewById(R.id.connexion_button);
+        connexion_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignInActivity();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void startSignInActivity() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers =
+                Arrays.asList(
+                        new AuthUI.IdpConfig.GoogleBuilder().build(),
+                        new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_baseline_group_24)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private void startMainActivity() {
+        Intent mainActivityIntent = new Intent(LoginActivity.this, MainActivity.class);
+        LoginActivity.this.startActivity(mainActivityIntent);
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                showToast(getString(R.string.connection_succeed));
+                startMainActivity();
+            } else {
+                // ERRORS
+                if (response == null) {
+                    showToast(getString(R.string.error_authentication_canceled));
+                } else if (response.getError() != null) {
+                    if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                        showToast(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showToast(getString(R.string.error_unknown_error));
+                    }
+                }
+            }
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+}
